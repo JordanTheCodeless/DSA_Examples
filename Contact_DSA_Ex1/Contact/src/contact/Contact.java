cf907b3490f8076724c61be87a0e5179ce887cf1
JordanTheCodeless <131815445+JordanTheCodeless@users.noreply.github.com>
2025-06-17 14:52:36 +0100

    Created basic structure with necessary classes needed for Contact application

---
+++ Contact_DSA_Ex1/Contact/src/contact/Contact.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package contact;
+
+/**
+ *
+ * @author jordancarthy
+ */
+public class Contact {
+    private String firstName, lastName, phone, email, address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" + "firstName=" + firstName + ", lastName=" + lastName + ", phone=" + phone + ", email=" + email + ", address=" + address + '}';
+    }
+    
+}
